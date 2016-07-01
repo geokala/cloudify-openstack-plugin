94b7555bf3cf05a8d0982dbf550fdd385fcc1803
Madda <nobody@nowhere.local>
2016-07-01 11:16:10 +0100

    Fix pip install --upgrade issue

--- system_tests/setup.py
+++ system_tests/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
+
+# We don't depend on cloudify-tester because otherwise a pip install --upgrade
+# will cause failures since it's not on pip
 setup(
     name="cloudify_tester_openstack_plugin",
     version="0.1.0",
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    install_requires=[
-        'cloudify_tester',
-    ],
 )
